674158a845a484555640e42b44fd966270c8a717
DaPhantomxx <alperendonmez@email.com>
2024-05-20 14:03:17 +0300

    updated db functions to receive latest datas

--- lib/dbhelper/mongodb.dart
+++ lib/dbhelper/mongodb.dart
@@ -2,7 +2,7 @@ import 'package:cengproject/dbhelper/constant.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class MongoDatabase {
-  static var db, caregiverCollection, patientCollection;
+  static var db;
 
   static Future<void> connect() async {
     try {
@@ -19,34 +19,6 @@ class MongoDatabase {
         print('Failed to retrieve server status');
         return;
       }
-
-      // Access the caregiver collection
-      try {
-        caregiverCollection = db!.collection(CAREGIVER_COLLECTION);
-        // Fetch and print documents in the caregiver collection
-        var caregivers = await caregiverCollection!.find().toList();
-        if (caregivers.isEmpty) {
-          print('Caregiver collection is empty');
-        } else {
-          print(caregivers);
-        }
-      } catch (e) {
-        print('An error occurred while accessing caregiver collection: $e');
-      }
-
-      // Access the patient collection
-      try {
-        patientCollection = db!.collection(PATIENT_COLLECTION);
-        // Fetch and print documents in the patient collection
-        var patients = await patientCollection!.find().toList();
-        if (patients.isEmpty) {
-          print('Patient collection is empty');
-        } else {
-          print(patients);
-        }
-      } catch (e) {
-        print('An error occurred while accessing patient collection: $e');
-      }
     } catch (e) {
       // Handle any errors that occur during connection or querying
       print('An error occurred: $e');
@@ -54,15 +26,12 @@ class MongoDatabase {
   }
 
   static Future<bool> authenticateUser(String username, String password) async {
-    //await connect(); // Ensure database connection
-
     try {
-      var user =
-          await caregiverCollection!.findOne(where.eq('username', username));
+      var collection = db!.collection(CAREGIVER_COLLECTION);
+      var user = await collection.findOne(where.eq('username', username));
       if (user != null) {
         var storedPassword = user['password'];
         if (storedPassword == password) {
-          // Simple password check
           return true;
         }
       }
@@ -73,27 +42,36 @@ class MongoDatabase {
   }
 
   static Future<Map<String, dynamic>?> getUser(String username) async {
-    var user = await caregiverCollection?.findOne({
-      'username': username,
-    });
-    return user;
+    try {
+      var collection = db!.collection(CAREGIVER_COLLECTION);
+      var user = await collection.findOne({'username': username});
+      return user;
+    } catch (e) {
+      print('An error occurred while getting user: $e');
+      return null;
+    }
   }
 
   static Future<List<Map<String, dynamic>>> getCarePatients(
       List<String> patientIds) async {
-    var objectIds = patientIds.map((id) => ObjectId.parse(id)).toList();
-    var patients = await patientCollection!
-        ?.find(where.oneFrom('_id', objectIds))
-        .toList();
-    return patients ?? [];
+    try {
+      var objectIds = patientIds.map((id) => ObjectId.parse(id)).toList();
+      var patients = await db!
+          .collection(PATIENT_COLLECTION)
+          .find(where.oneFrom('_id', objectIds))
+          .toList();
+      return patients;
+    } catch (e) {
+      print('An error occurred while getting care patients: $e');
+      return [];
+    }
   }
 
   static Future<bool> createUser(String username, String password) async {
-    //await connect();
-
     try {
-      var existingUser =
-          await caregiverCollection!.findOne(where.eq('username', username));
+      var existingUser = await db!
+          .collection(CAREGIVER_COLLECTION)
+          .findOne(where.eq('username', username));
       if (existingUser != null) {
         return false; // User already exists
       }
@@ -104,11 +82,43 @@ class MongoDatabase {
         'care_patients': []
       };
 
-      await caregiverCollection!.insertOne(newUser);
+      await db!.collection(CAREGIVER_COLLECTION).insertOne(newUser);
       return true;
     } catch (e) {
       print('An error occurred during user creation: $e');
       return false;
+    }
+  }
+
+  // this part will be fixed and adjusted so that if patient is not found and stuff aw geez
+  static Future<void> addPatient(
+      String caregiverId, String patientNumber) async {
+    try {
+      var caregiverCollection = db!.collection(CAREGIVER_COLLECTION);
+      var patientCollection = db!.collection(PATIENT_COLLECTION);
+
+      // Check if the patient exists
+      var patient =
+          await patientCollection.findOne({'patient_number': patientNumber});
+      if (patient == null) {
+        print('Patient does not exist.');
+        return;
+      }
+
+      var patientId = patient['_id'] as ObjectId;
+
+      // Convert ObjectId to string
+      // ignore: deprecated_member_use
+      String patientIdString = patientId.toHexString();
+
+      // Update the caregiver's array of care_patients with ObjectId as string
+      await caregiverCollection.updateOne(
+        where.eq('_id', ObjectId.fromHexString(caregiverId)),
+        modify.push('care_patients', patientIdString),
+      );
+      print('Patient added to caregiver\'s care_patients list.');
+    } catch (e) {
+      print('Error adding patient: $e');
     }
   }
 
