8d8aabce558a88bf1acfc598e4a13db41ac728af
DaPhantomxx <alperendonmez@email.com>
2024-05-29 02:21:46 +0300

    ui changes

--- lib/addExercise_page.dart
+++ lib/addExercise_page.dart
@@ -10,39 +10,70 @@ class AddExercisesPage extends StatefulWidget {
 
 class _AddExercisesPageState extends State<AddExercisesPage> {
   final Map<String, int> _exerciseReps = {};
+  final List<Map<String, dynamic>> _exercises = [
+    {'name': 'Arm Raise (Left)', 'type': 'Arms'},
+    {'name': 'Arm Raise (Right)', 'type': 'Arms'},
+    {'name': 'Open Arms', 'type': 'Arms'},
+    {'name': 'Squat', 'type': 'Legs'},
+    {'name': 'Leg Raise (Left)', 'type': 'Legs'},
+    {'name': 'Leg Raise (Right)', 'type': 'Legs'},
+    {'name': 'Neck Flexion (Left)', 'type': 'Neck'},
+    {'name': 'Neck Flexion (Right)', 'type': 'Neck'},
+    {'name': 'Default Exercise 1', 'type': 'Default'},
+    {'name': 'Default Exercise 2', 'type': 'Default'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Exercises'),
+        backgroundColor: const Color.fromARGB(255, 34, 43, 170),
+        title: const Text('Add Exercises', style: TextStyle(color: Colors.white)),
+        iconTheme: const IconThemeData(color: Colors.white),
         actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0), // Add padding to the right
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'ASSIGN',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: [
-          _exerciseCard('Arm Raise (Left)'),
-          _exerciseCard('Arm Raise (Right)'),
-          _exerciseCard('Open Arms'),
-          _exerciseCard('Squat'),
-          _exerciseCard('Leg Raise (Left)'),
-          _exerciseCard('Leg Raise (Right)'),
-          _exerciseCard('Neck Flexion (Left)'),
-          _exerciseCard('Neck Flexion (Right)'),
-        ],
+      backgroundColor: const Color.fromARGB(255, 34, 43, 170),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 3 / 2,
+          ),
+          itemCount: _exercises.length,
+          itemBuilder: (context, index) {
+            final exercise = _exercises[index];
+            return _exerciseCard(exercise['name'], exercise['type']);
+          },
+        ),
       ),
     );
   }
 
-  Widget _exerciseCard(String exerciseName) {
+  Widget _exerciseCard(String exerciseName, String exerciseType) {
     return Card(
+      color: Colors.white.withOpacity(0.3),
       margin: const EdgeInsets.all(10),
       elevation: 7,
       shape: RoundedRectangleBorder(
@@ -52,27 +83,44 @@ class _AddExercisesPageState extends State<AddExercisesPage> {
         onTap: () {
           _showRepsDialog(context, exerciseName);
         },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              exerciseName,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            _exerciseReps.containsKey(exerciseName)
-                ? Text(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Text(
+                  exerciseName,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Flexible(
+                child: Text(
+                  exerciseType,
+                  style: const TextStyle(fontSize: 12, color: Colors.white70),
+                ),
+              ),
+              const SizedBox(height: 5),
+              if (_exerciseReps.containsKey(exerciseName))
+                Flexible(
+                  child: Text(
                     '${_exerciseReps[exerciseName]} reps',
-                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  )
-                : IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () {
-                      _showRepsDialog(context, exerciseName);
-                    },
-                    iconSize: 40,
+                    style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),
                   ),
-          ],
+                )
+              else
+                IconButton(
+                  icon: const Icon(Icons.add, color: Colors.white),
+                  onPressed: () {
+                    _showRepsDialog(context, exerciseName);
+                  },
+                  iconSize: 24,
+                ),
+            ],
+          ),
         ),
       ),
     );
