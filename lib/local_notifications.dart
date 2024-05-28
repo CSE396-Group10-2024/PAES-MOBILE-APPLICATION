7de7c544d1bdfb1f93a33a23e9956d3f868190e4
DaPhantomxx <alperendonmez@email.com>
2024-05-29 02:19:09 +0300

    changed notification icon

--- lib/local_notifications.dart
+++ lib/local_notifications.dart
@@ -7,7 +7,7 @@ class LocalNotifications {
   static Future init() async {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
+        AndroidInitializationSettings('@mipmap/launcher_icon');
     const initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
 
     flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
